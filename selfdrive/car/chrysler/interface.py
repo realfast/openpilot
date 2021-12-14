704778e05dec421915edb204dd013b7f7a65539e
realfast <jraycec@gmail.com>
2021-12-14 17:07:39 -0600

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -48,7 +48,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3254. + STD_CARGO_KG  # kg curb weight 2021 Ram 2500
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[0.], [0.,]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15], [0.015,]]
-      ret.steerActuatorDelay = 0.25
+      ret.steerActuatorDelay = 0.2
       ret.steerRateCost = 1.0  # may need tuning
       ret.centerToFront = ret.wheelbase * 0.4 # just a guess
       ret.minSteerSpeed = 15.65
