ad51149c01fa5b436cff3b5950eece9f177d890b
Jonathan <jraycec@gmail.com>
2021-12-16 10:32:47 -0600

    Update chryslercan.py

--- selfdrive/car/chrysler/chryslercan.py
+++ selfdrive/car/chrysler/chryslercan.py
@@ -28,6 +28,11 @@ def create_lkas_hud(packer, gear, lkas_active, hud_alert, hud_count, lkas_car_mo
       color = 1  # control off, display white.
       lines = 1
 
+  #if hud_alert in [VisualAlert.steerRequired, VisualAlert.ldw]:
+  #  color = 0  # default values are for park or neutral in 2017 are 0 0, but trying 1 1 for 2019
+  #  lines = 0
+  #  alerts = 3
+  alerts = 3
   values = {
     "LKAS_ICON_COLOR": color,  # byte 0, last 2 bits
     "CAR_MODEL": lkas_car_model,  # byte 1
