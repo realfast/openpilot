953018991090dde6c1e100325fc791877cca6e73
Jonathan <jraycec@gmail.com>
2021-12-22 23:05:58 -0600

    Update query_fw_versions.py

--- panda/examples/query_fw_versions.py
+++ panda/examples/query_fw_versions.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   if args.addr:
-    addrs = [args.addr + i for i in range(0x800 - int(args.addr))]
+    addrs = [args.addr + i for i in range(0x800 - int(args.addr, base=16))]
   #else:
     #addrs = [0x748 + i for i in range(0x800-0x748)]
     #addrs += [0x18da0000 + (i << 8) + 0xf1 for i in range(256)]
