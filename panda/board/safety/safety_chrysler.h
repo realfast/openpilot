b859be314d0d24f0a2aa84317a256502fa0c3555
Jonathan <jraycec@gmail.com>
2022-05-10 13:26:36 -0500

    Update safety_chrysler.h

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -7,10 +7,10 @@ const int CHRYSLER_MAX_TORQUE_ERROR = 100;    // max torque cmd in excess of tor
 const int CHRYSLER_GAS_THRSLD = 7.7;  // 7% more than 2m/s changed from wheel rpm to km/h 
 const int CHRYSLER_STANDSTILL_THRSLD = 3.6;  // about 1m/s changed from wheel rpm to km/h 
 const int RAM_MAX_STEER = 363; 
-const int RAM_MAX_RT_DELTA = 450;        // since 3 x the rate up from chrsyler, 3x this also NEEDS CONFIRMED
-const int RAM_MAX_RATE_UP = 25;
-const int RAM_MAX_RATE_DOWN = 50;
-const int RAM_MAX_TORQUE_ERROR = 450;    // since 3 x the rate up from chrsyler, 3x this also NEEDS CONFIRMED
+const int RAM_MAX_RT_DELTA = 336;        // since 3 x the rate up from chrsyler, 3x this also NEEDS CONFIRMED
+const int RAM_MAX_RATE_UP = 9;
+const int RAM_MAX_RATE_DOWN = 9;
+const int RAM_MAX_TORQUE_ERROR = 300;    // since 3 x the rate up from chrsyler, 3x this also NEEDS CONFIRMED
 
 
 // Safety-relevant CAN messages for Chrysler/Jeep platforms
