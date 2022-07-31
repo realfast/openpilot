49b0444e1922fc9b7fadc8aafb2dd83dc72e3202
Jonathan <jraycec@gmail.com>
2022-07-31 15:14:02 -0500

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -53,11 +53,13 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 2493. + STD_CARGO_KG
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
-      ret.minSteerSpeed = 1.0
+      ret.minSteerSpeed = 14.5
       if car_fw is not None:
         for fw in car_fw:
           if fw.ecu == 'eps' and fw.fwVersion in (b"68312176AE", b"68312176AG", b"68273275AG"):
             ret.minSteerSpeed = 0.
+          elif fw.ecu == 'eps' and fw.fwVersion in (b"68466110AB"):
+            ret.minSteerSpeed = 1.0
 
     else:
       raise ValueError(f"Unsupported car: {candidate}")
