be8196f12f7e51de5c9b3aca811f43921e94bd37
Jonathan <jraycec@gmail.com>
2022-08-01 16:36:15 -0500

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -3,7 +3,9 @@ from common.realtime import DT_CTRL
 from selfdrive.car import apply_toyota_steer_torque_limits
 from selfdrive.car.chrysler.chryslercan import create_lkas_hud, create_lkas_command, create_cruise_buttons
 from selfdrive.car.chrysler.values import CAR, RAM_CARS, CarControllerParams
+from cereal import car
 
+GearShifter = car.CarState.GearShifter
 
 class CarController:
   def __init__(self, dbc_name, CP, VM):
@@ -14,23 +16,28 @@ class CarController:
     self.last_lkas_falling_edge = 0
     self.lkas_control_bit_prev = False
     self.last_button_frame = 0
-
+    
     self.packer = CANPacker(dbc_name)
     self.params = CarControllerParams(CP)
 
   def update(self, CC, CS):
     can_sends = []
-
+    
+    
     # TODO: can we make this more sane? why is it different for all the cars?
     lkas_control_bit = self.lkas_control_bit_prev
-    if CS.out.vEgo > self.CP.steerMinActivation:
+
+    CS.out.gearShifter
+    if CS.out.vEgo >= self.CP.steerMinActivation and CS.out.gearShifter == GearShifter.drive:
       lkas_control_bit = True
+    elif CS.out.gearShifter != GearShifter.drive:
+      lkas_control_bit = False
     elif self.CP.carFingerprint in (CAR.PACIFICA_2019_HYBRID, CAR.PACIFICA_2020, CAR.JEEP_CHEROKEE_2019):
       if CS.out.vEgo < (self.CP.minSteerSpeed - 3.0):
         lkas_control_bit = False
-    elif self.CP.carFingerprint in RAM_CARS:
-      if CS.out.vEgo < (self.CP.minSteerSpeed - 0.5):
-        lkas_control_bit = False
+    # elif self.CP.carFingerprint in RAM_CARS:
+    #   if CS.out.vEgo < (self.CP.minSteerSpeed - 0.5):
+    #     lkas_control_bit = False
 
     # EPS faults if LKAS re-enables too quickly
     lkas_control_bit = lkas_control_bit and (self.frame - self.last_lkas_falling_edge > 200)
