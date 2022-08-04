4d4f53bebd9d63970f0d2514417419e0d8664230
Jonathan <jraycec@gmail.com>
2022-08-04 07:58:54 -0500

    Revert Back to commit after it last worked correctly

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -1,14 +1,13 @@
 from cereal import car
-from opendbc.can.packer import CANPacker
-from common.realtime import DT_CTRL
 from selfdrive.car import apply_toyota_steer_torque_limits
 from selfdrive.car.chrysler.chryslercan import create_lkas_hud, create_lkas_command, create_cruise_buttons
 from selfdrive.car.chrysler.values import CAR, CarControllerParams
+from opendbc.can.packer import CANPacker
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
     self.apply_steer_last = 0
-    self.frame = 0
+    self.lkasframe = 0
     self.prev_frame = -1
     self.hud_count = 0
     self.frame = 0
@@ -21,7 +20,7 @@ class CarController():
     #self.CarControllerParams = CarControllerParams
 
     self.packer = CANPacker(dbc_name)
-    self.params = CarControllerParams()
+    # self.params = CarControllerParams(CP)
 
   def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, hud_alert,
              left_line, right_line, lead, left_lane_depart, right_lane_depart):
@@ -49,12 +48,12 @@ class CarController():
       #self.gone_fast_yet = CS.out.vEgo > CS.CP.minSteerSpeed
 
     if self.gone_fast_yet_previous == True and self.gone_fast_yet == False:
-        self.lkaslast_frame = self.frame
+        self.lkaslast_frame = self.lkasframe
 
     #if CS.out.steerFaultPermanent is True: #possible fix for LKAS error Plan to test
     #  gone_fast_yet = False
 
-    if (CS.out.steerFaultPermanent is True) or (self.frame-self.lkaslast_frame<400):#If the LKAS Control bit is toggled too fast it can create and LKAS error
+    if (CS.out.steerFaultPermanent is True) or (self.lkasframe-self.lkaslast_frame<400):#If the LKAS Control bit is toggled too fast it can create and LKAS error
       self.gone_fast_yet = False
 
     lkas_active = self.gone_fast_yet and enabled
@@ -72,29 +71,32 @@ class CarController():
 
     # LKAS_HEARTBIT is forwarded by Panda so no need to send it here.
     # frame is 100Hz (0.01s period)
-    if (self.frame % 25 == 0):  # 0.25s period
+    if (self.lkasframe % 25 == 0):  # 0.25s period
       if (CS.lkas_car_model != -1):
-        can_sends.append(create_lkas_hud(
-            self.packer, self.params, lkas_active, hud_alert, self.hud_count, CS.lkas_car_model, CS.auto_high_beam))
+        new_msg = create_lkas_hud(
+            self.packer, CarControllerParams, lkas_active, hud_alert, self.hud_count, CS.lkas_car_model, CS.auto_high_beam)
+        can_sends.append(new_msg)
         self.hud_count += 1
 
     if self.frame % 2 == 0:
       # steer torque
-      new_steer = int(round(actuators.steer * self.params.STEER_MAX))
+      new_steer = int(round(actuators.steer * CarControllerParams.STEER_MAX))
       apply_steer = apply_toyota_steer_torque_limits(new_steer, self.apply_steer_last,
-                                                    CS.out.steeringTorqueEps, self.params)
+                                                    CS.out.steeringTorqueEps, CarControllerParams)
       self.steer_rate_limited = new_steer != apply_steer
       if not lkas_active or self.gone_fast_yet_previous == False:
         apply_steer = 0
       self.apply_steer_last = apply_steer
       self.gone_fast_yet_previous = self.gone_fast_yet
 
-      can_sends.append(create_lkas_command(self.packer, self.params, int(apply_steer), self.gone_fast_yet, frame))
+      new_msg = create_lkas_command(self.packer, CarControllerParams, int(apply_steer), self.gone_fast_yet, frame)
+      can_sends.append(new_msg)
     
 
     self.prev_frame = frame
+    self.lkasframe += 1
     self.frame += 1
     new_actuators = actuators.copy()
-    new_actuators.steer = self.apply_steer_last / self.params.STEER_MAX
+    new_actuators.steer = self.apply_steer_last  / CarControllerParams.STEER_MAX
 
     return new_actuators, can_sends
