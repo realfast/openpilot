8bc7275fc71f1c8d1481770bf138db635d8919b5
Jonathan <jraycec@gmail.com>
2022-08-14 21:21:38 -0500

    friction issue

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -53,7 +53,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 16.3
       ret.mass = 2493. + STD_CARGO_KG
       ret.maxLateralAccel = 2.5
-      ret.lateralTuning.torque.friction = 0.05
+      # ret.lateralTuning.torque.friction = 0.05
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
       ret.minSteerSpeed = 14.6
