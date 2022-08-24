212e6593fc6c27f8c80806724da749b4e8819433
Jonathan <jraycec@gmail.com>
2022-08-24 07:38:16 -0500

    Update query_fw_versions.py

--- panda/examples/query_fw_versions.py
+++ panda/examples/query_fw_versions.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
   # for std_id in DATA_IDENTIFIER_TYPE:
   #   uds_data_ids[std_id.value] = std_id.name
   if args.nonstandard:
-    for uds_id in range(0xf100,0xffff):
+    for uds_id in range(0x0000,0xffff):
       uds_data_ids[uds_id] = "IDENTIFICATION_OPTION_VEHICLE_MANUFACTURER_SPECIFIC_DATA_IDENTIFIER"
     # for uds_id in range(0xf1a0,0xf1f0):
     #   uds_data_ids[uds_id] = "IDENTIFICATION_OPTION_VEHICLE_MANUFACTURER_SPECIFIC"
