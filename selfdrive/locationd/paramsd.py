1fbda6f053c367c4c988b18e2d1e5fb02588e877
Jonathan <jraycec@gmail.com>
2022-10-25 14:48:50 -0500

    Update paramsd.py

--- selfdrive/locationd/paramsd.py
+++ selfdrive/locationd/paramsd.py
@@ -192,7 +192,7 @@ def main(sm=None, pm=None):
       liveParameters.angleOffsetAverageDeg = angle_offset_average
       liveParameters.angleOffsetDeg = angle_offset
       liveParameters.valid = all((
-        abs(liveParameters.angleOffsetAverageDeg) < 10.0,
+        # abs(liveParameters.angleOffsetAverageDeg) < 10.0,
         # abs(liveParameters.angleOffsetDeg) < 10.0,
         # 0.2 <= liveParameters.stiffnessFactor <= 5.0,
         # min_sr <= liveParameters.steerRatio <= max_sr,
