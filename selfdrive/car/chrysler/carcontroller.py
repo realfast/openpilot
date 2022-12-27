c24004dc0ab374bb10c4e666be9b6945609b9c9a
realfast <jraycec@gmail.com>
2022-12-27 07:59:09 -0600

    TESTING torque

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -92,70 +92,73 @@ class CarController:
       self.lkas_control_bit_prev = lkas_control_bit
 
       can_sends.append(create_lkas_command(self.packer, self.CP, int(apply_steer), lkas_control_bit))
+      #LONG
+      das_3_counter = CS.das_3['COUNTER']
+
+      if not CC.enabled:
+        self.last_brake = None
+
+      max_gear = 8
+
+      self.accel = clip(CC.actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
+
+      if CC.actuators.accel <= 0:
+        accel_req = False
+        decel_req = False
+        torque = None
+        decel = self.acc_brake(self.accel)
+        max_gear = 8
+        delta_accel = 0
+      else:
+        self.last_brake = None
+        accel_req = True
+        decel_req = False
+        # delta_accel = CC.actuators.accel - CS.out.aEgo
+          
+          # adding a factor to the velocity. 1.0 multiplied by the delta_accel assumes that we are telling the formula we want to be x m/s faster than we currently
+          # are 1 second from now. I add the ability to modify this in real time to dial it in better. 
+          # For example if comma is requesting 2.0m/s2 acceleration and we use a 1.0 multiplier, then we are saying in 1 second we want to be traveling 2.0m/s faster. 
+          # if we put a 1.5 multiplier then it becomes 3.0 m/s faster 1 second from now. 
+        # if delta_accel < 0: 
+        #   velocity = clip(abs(delta_accel),  -3.5, 2.0)
+        # else:
+        #   velocity = clip(abs(delta_accel),  -3.5, 2.0)
+
+        #calculate torque using self.CP.mass, self.accel, CS.vEgoRaw, CS.engineTorque, and CS.engineRPM
+        # torque = ((self.CP.mass * self.accel)  / (CS.out.vEgoRaw + .00001))*.020 #+ .00001 to prevent divide by zero
+        # torque = Power (W) / (RPM * 2 * pi / 60)
+        # Power (W)= work(J) * time (s)
+        # work (J) = force (N) * distance (m)
+        # force (N) = mass (kg) * acceleration (m/s^2)
+        # distance (m) =  (acceleration(m/s^2) * time(s)^2 / 2) + velocity(m/s)
+        
+        distancemoved = (.5 self.accel * 1.0^2) + (CS.out.vEgoRaw * 1.0)
+        torque = (self.CP.mass * self.accel * distancemoved * 9.55414)/CS.EngineRPM
+
+        # torque = (self.CP.mass*self.accel*CS.out.vEgoRaw) / (CS.EngineRPM + .00001) #+ .00001 to prevent divide by zero
+
+        if CS.engineTorque < 0 and torque > 0:
+          torque += 0
+
+        #If torque is positive, add the engine torque to the torque we calculated. This is because the engine torque is the torque the engine is producing.
+        else:
+          torque += CS.engineTorque
+
+        decel = None
+
+      can_sends.append(acc_command(self.packer, das_3_counter, CC.enabled,
+                                    accel_req,
+                                    torque,
+                                    max_gear,
+                                    decel_req,
+                                    decel,
+                                    CS.das_3))
 
     # HUD alerts
     if self.frame % 25 == 0:
       if CS.lkas_car_model != -1:
         can_sends.append(create_lkas_hud(self.packer, self.CP, lkas_active, CC.hudControl.visualAlert, self.hud_count, CS.lkas_car_model, CS))
         self.hud_count += 1
-
-    #LONG
-    das_3_counter = CS.das_3['COUNTER']
-
-    if not CC.enabled:
-      self.last_brake = None
-
-    max_gear = 8
-
-    self.accel = clip(CC.actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
-
-    if CC.actuators.accel <= 0:
-      accel_req = False
-      decel_req = False
-      torque = None
-      decel = self.acc_brake(self.accel)
-      max_gear = 8
-      delta_accel = 0
-    else:
-      self.last_brake = None
-      accel_req = True
-      decel_req = False
-      # delta_accel = CC.actuators.accel - CS.out.aEgo
-        
-        # adding a factor to the velocity. 1.0 multiplied by the delta_accel assumes that we are telling the formula we want to be x m/s faster than we currently
-        # are 1 second from now. I add the ability to modify this in real time to dial it in better. 
-        # For example if comma is requesting 2.0m/s2 acceleration and we use a 1.0 multiplier, then we are saying in 1 second we want to be traveling 2.0m/s faster. 
-        # if we put a 1.5 multiplier then it becomes 3.0 m/s faster 1 second from now. 
-      # if delta_accel < 0: 
-      #   velocity = clip(abs(delta_accel),  -3.5, 2.0)
-      # else:
-      #   velocity = clip(abs(delta_accel),  -3.5, 2.0)
-
-      #calculate torque using self.CP.mass, self.accel, CS.vEgoRaw, CS.engineTorque, and CS.engineRPM
-      torque = (self.CP.mass * self.accel)  / (CS.out.vEgoRaw + .00001) #+ .00001 to prevent divide by zero
-
-      # torque = (self.CP.mass*self.accel*CS.out.vEgoRaw) / (CS.EngineRPM + .00001) #+ .00001 to prevent divide by zero
-
-      if CS.engineTorque < 0 and torque > 0:
-        #If torque is less than -200nm, we need to get back to 0 quickly.
-        if CS.engineTorque <= -200:
-          torque = 0
-        #A truck will idle between 100-200nm So, If torque is very negative we need to get back to positive quickly. 
-        else:
-          torque +=200 
-      #If torque is positive, add the engine torque to the torque we calculated. This is because the engine torque is the torque the engine is producing.
-      else:
-        torque += CS.engineTorque
-
-      decel = None
-
-    can_sends.append(acc_command(self.packer, das_3_counter, CC.enabled,
-                                   accel_req,
-                                   torque,
-                                   max_gear,
-                                   decel_req,
-                                   decel,
-                                   CS.das_3))
 
     self.frame += 1
 
