ee328136785b7fb3dd492bc92808d873302ace9b
realfast <jraycec@gmail.com>
2023-01-06 03:36:07 -0600

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -104,10 +104,11 @@ class CarController:
       max_gear = 8
 
       self.accel = clip(CC.actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
-      brake_threshold = - self.op_params.get('brake_threshold')
-      if CS.out.vEgo < 2.25:
-        brake_threshold = 0
-      if CC.actuators.accel < - brake_threshold:
+      brake_threshold = 0
+      if CS.out.vEgo > 2.25:
+        brake_threshold -= self.op_params.get('brake_threshold')
+        
+      if CC.actuators.accel < brake_threshold:
         accel_req = False
         decel_req = False
         torque = None
