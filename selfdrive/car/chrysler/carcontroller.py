70942a1f204709ad0eb4f68c41387f9e0e40ebd5
realfast <jraycec@gmail.com>
2024-08-14 07:15:28 -0500

    moving things around

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -67,10 +67,6 @@ class CarController(CarControllerBase):
       # EPS faults if LKAS re-enables too quickly
       lkas_control_bit = lkas_control_bit and (self.frame - self.last_lkas_falling_edge > 200)
 
-      if not lkas_control_bit and self.lkas_control_bit_prev:
-        self.last_lkas_falling_edge = self.frame
-      self.lkas_control_bit_prev = lkas_control_bit
-
       # steer torque
       new_steer = int(round(CC.actuators.steer * self.params.STEER_MAX))
       apply_steer = apply_meas_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorqueEps, self.params)
@@ -78,10 +74,16 @@ class CarController(CarControllerBase):
         apply_steer = 0
 
       # Speed spoofing logic
-      if lkas_active and CS.out.vEgoRaw * CV.MS_TO_MPH < 36:
+      if CC.enabled and not low_speed_alert and CS.out.vEgoRaw * CV.MS_TO_MPH < 36 and self.spoof_speed < 36 * CV.MPH_TO_KPH:
         self.spoof_speed = 36 * CV.MPH_TO_KPH
+        lkas_control_bit = False
       else:
         self.spoof_speed = CS.out.vEgoRaw * CV.MS_TO_KPH
+
+
+      if not lkas_control_bit and self.lkas_control_bit_prev:
+        self.last_lkas_falling_edge = self.frame
+      self.lkas_control_bit_prev = lkas_control_bit
 
       can_sends.append(chryslercan.create_lkas_command(self.packer, self.CP, int(apply_steer), lkas_control_bit, self.frame, 0))
       can_sends.append(chryslercan.create_lkas_command(self.packer, self.CP, int(apply_steer), lkas_control_bit, self.frame, 1))
